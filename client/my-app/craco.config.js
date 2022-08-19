3411707c10539046fba43edcd5f18ba11b42068d
Thanh Le <thanhlevan0203@gmail.com>
2022-08-18 19:19:47 -0700

    added tailwin.config.js file

--- client/my-app/craco.config.js
+++ client/my-app/craco.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    mode: 'development',
-    // Adding Server
-    devServer: {
-        port: 8001,
+    style: {
+        postcssOptions: {
+            plugins: [require('tailwindcss'), require('autoprefixer')],
+        },
     },
-};
+}
